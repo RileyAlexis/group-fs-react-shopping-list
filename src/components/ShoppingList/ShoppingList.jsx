0299a0cbf454e5e92a62db723e49f482a970fb75
RileyAlexis <MxRileyAlexis@gmail.com>
2023-08-28 11:33:05 -0500

    Shopping List styled with Material UI

--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -3,6 +3,18 @@ import axios from 'axios';
 import swal from 'sweetalert';
 
 
+//Material UI
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+
+
 function ShoppingList({fetchList, item}) {
   
     const buyItem = () => {
@@ -38,21 +50,58 @@ function ShoppingList({fetchList, item}) {
         )
       }
 
-            
-
       return (
+        <Container maxWidth="lg" disableGutters >
+        <Card className={item.complete ? 'completed' : ''} variant="outlined" 
+            sx={{ 
+                display: 'flex', 
+                maxWidth:275, 
+                gap:8, 
+                mb:2,
+                boxShadow: 1,
+                borderRadius: 2,
+                p: 2
+                }}>
+           <Box sx={{display: 'flex'}}>
+          <CardContent >
+          <Typography variant='h5' color="text.Primary" gutterBottom>
+            {item.item}
+          </Typography>
+          <Typography sx={{ fontsize: 12 }} color="text.secondary">
+            Qty: {item.quantity} - {item.unit}
+          </Typography>
+          </CardContent>
+            <CardActions >
+            <IconButton onClick={buyItem} color="primary" aria-label="add to shopping cart">
+        <AddShoppingCartIcon />
+      </IconButton>
+            <IconButton onClick={deleteItem} color="warning" aria-label="Delete item">
+              <DeleteIcon />
+            </IconButton>
+
+
+
+            </CardActions>
+
+        </Box>
+        </Card>
+        </Container>
         
-        <div className={`item-box ${item.complete ? 'completed' : ''}`} >
-          <div className="item-box-text">
-          <p>{item.item}</p>
-          <p>Qty: {item.quantity}, {item.unit}</p>
-          <p>{item.complete}</p>
-          </div>
-          <div className="item-box-btn-box">
-              <button className="buyBtn" onClick={buyItem}>Buy</button> 
-          <button className="deleteBtn" onClick={deleteItem}>X</button>
-          </div>
-        </div>
+
+
+
+
+        // <div className={`item-box ${item.complete ? 'completed' : ''}`} >
+        //   <div className="item-box-text">
+        //   <p>{item.item}</p>
+        //   <p>Qty: {item.quantity}, {item.unit}</p>
+        //   <p>{item.complete}</p>
+        //   </div>
+        //   <div className="item-box-btn-box">
+        //       <button className="buyBtn" onClick={buyItem}>Buy</button> 
+        //   <button className="deleteBtn" onClick={deleteItem}>X</button>
+        //   </div>
+        // </div>
           
       )
             }
